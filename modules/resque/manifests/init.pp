33c4668e783c0d7c23539db980fe5ea08a9ee09a
Jason Mahony <jason@verticalresponse.com>
2014-11-16 02:38:10 -0800

    working on resque manifest

--- modules/resque/manifests/init.pp
+++ modules/resque/manifests/init.pp
@@ -2,7 +2,7 @@ class resque ( $rails_env ) {
   
   $source = "puppet:///modules/resque"
 
-  # Passing parameters to a file template
+  # Creating worker defined type
   define worker ( $resque_queue, $rails_env ) {
      file { "/etc/init/${title}.conf":
      mode    => 644, 
@@ -11,13 +11,24 @@ class resque ( $rails_env ) {
      }
   }
 
-  # Call the file template with set parameters
+  # Call the worker type and pass in parameters
   worker { 'resque-event': resque_queue => "event", rails_env => "${rails_env}" }
   worker { 'resque-signin': resque_queue => "signin", rails_env => "${rails_env}" }
 
-  # Ensure the resque init script, resque-scheduler upstart and resque gem are installed    
+  # Ensure the resque init script and resque gem are installed    
   file { '/etc/init.d/resque': mode => 755, ensure => present, source => "$source/resque-init" }
-  file { '/etc/init/resque-scheduler.conf': mode => 0644, ensure => present, template => "resque/resque-scheduler.conf.erb" }
   package { 'resque': ensure => 'installed', provider => 'gem' }
 
+  # Simple defined type for resque scheduler
+  define scheduler ( $rails_env ) {
+    file { '/etc/init/resque-scheduler.conf':
+    mode => 0644,
+    ensure => present,
+    template => "resque/resque-scheduler.conf.erb"
+    }
+  }
+  
+  # Call the resque scheduler
+  scheduler { 'scheduler': rails_env => "${rails_env}"}
+  
 }
