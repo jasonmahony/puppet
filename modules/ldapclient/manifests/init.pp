b0697e3c04da935d9a89628176b1734d10c4877b
Jason Mahony <jason@verticalresponse.com>
2014-11-16 03:25:02 -0800

    adding network module to common; other clean up

--- modules/ldapclient/manifests/init.pp
+++ modules/ldapclient/manifests/init.pp
@@ -2,7 +2,7 @@ class ldapclient {
   $pkgs = [ "nss-pam-ldapd", "openldap", "openldap-clients" ]
   File { owner => root, group => root, mode => 0644 }
 
-  $ldapconfig     = "ldapclient/pam_ldap.conf"
+  $ldapconfig  = "ldapclient/pam_ldap.conf"
   
   package { $pkgs: 
     ensure => latest,
@@ -14,21 +14,13 @@ class ldapclient {
       '/etc/pam.d/password-auth' ] 
   }
   
-  file { "/etc/nsswitch.conf":     source => "puppet:///modules/ldapclient/nsswitch.conf" }
+  file { "/etc/nsswitch.conf": source => "puppet:///modules/ldapclient/nsswitch.conf" }
   file { "/etc/pam.d/system-auth-ac": source => "puppet:///modules/ldapclient/system-auth-ac" }
   file { "/etc/pam.d/password-auth-ac": source => "puppet:///modules/ldapclient/password-auth-ac" }
   file { "/etc/nslcd.conf": source => 'puppet:///modules/ldapclient/nslcd.conf' }
   file { '/etc/pam.d/system-auth': ensure => 'link', target => '/etc/pam.d/system-auth-ac'}
   file { '/etc/pam.d/password-auth': ensure => 'link', target => '/etc/pam.d/password-auth-ac'}
-  
-  file {
-    "/etc/pam_ldap.conf": 
-      content => template($ldapconfig), 
-      require => Package[ 
-        "nss-pam-ldapd", 
-        "openldap" 
-      ]
-  }
+  file { "/etc/pam_ldap.conf": content => template($ldapconfig), require => Package[ "nss-pam-ldapd", "openldap" ] }
   
   service { 'nslcd': enable => true, ensure => 'running' }
 }
--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,11 +1,5 @@
 class network {
- 
-  case $network_eth0 {
-    'w.x.y.z': { $gateway = "w.x.y.a" }  
-    'a.b.c.d': { $gateway = "a.b.c.z" }
-    default: { $gateway = "x.x.x.x" }
-  }
- 
+
   file {'/etc/sysconfig/network-scripts/ifcfg-eth0': 
       ensure => present,
       content => template("network/ifcfg-eth0")  
@@ -18,7 +12,7 @@ class network {
 
   exec { "network_restart":
     command        => "/etc/init.d/network restart",
-    path           => ["/usr/bin", "/usr/sbin", "/sbin", "/bin", "/usr/local/bin", "/usr/local/sbin", "/usr/local/rvm/bin/"],
+    path           => ["/usr/bin", "/usr/sbin", "/sbin", "/bin", "/usr/local/bin", "/usr/local/sbin" ],
     subscribe      => File["/etc/sysconfig/network-scripts/ifcfg-eth0"],
     refreshonly    => true
   }
