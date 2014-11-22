a321e445a607c6f2d509fac946752ee9d183450a
Jason Mahony <jason@verticalresponse.com>
2014-11-22 14:59:12 -0800

    allow virtual false for mysql packages

--- modules/mysql/manifests/client/install.pp
+++ modules/mysql/manifests/client/install.pp
@@ -3,6 +3,7 @@ class mysql::client::install {
   package { 'mysql_client':
     ensure => $mysql::client::package_ensure,
     name   => $mysql::client::package_name,
+    allow_virtual => false
   }
 
 }
