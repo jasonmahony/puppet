31a9b9a158dbea4752784d6bafad0eea734d9853
Jason Mahony <jason@verticalresponse.com>
2014-11-23 14:56:26 -0800

    updated ntp module

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,12 +1,11 @@
 class ntp {
   
-  $source = 'puppet:///modules/ntp'
   Package { ensure => latest }
   File { ensure => present, owner => root, group => root, mode => 644, require => Package['ntp'], backup => '.puppetbak' }
   
   package { 'ntp': allow_virtual => false }
 
-  file { '/etc/ntp.conf': source => "${source}/ntp.conf" }
+  file { '/etc/ntp.conf': content => template('ntp/ntp.conf') }
   file { '/etc/ntp/step-tickers': content => template('ntp/steptickers') }
   
   service { 'ntpd':
